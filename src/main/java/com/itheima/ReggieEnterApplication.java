7b5497c8002b5ed1c88631f90d220ee6b86f3f67
mintlove8023 <2210648858@qq.com>
2022-05-11 11:06:50 +0800

    添加 reggie_project 添加项目结构与前端静态资源

---
+++ src/main/java/com/itheima/ReggieEnterApplication.java
@@ -0,0 +1,19 @@
+package com.itheima;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 小空
+ * @create 2022-05-11 10:49
+ * @description 瑞吉点餐 Application 入口
+ */
+@SpringBootApplication
+@Slf4j
+public class ReggieEnterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieEnterApplication.class, args);
+        log.info("Reggie Application Start Success!");
+    }
+}
