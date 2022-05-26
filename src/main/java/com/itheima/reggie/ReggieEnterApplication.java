fe3fab4645cde67bcf331f1cd69f0fca03e51855
mintlove8023 <2210648858@qq.com>
2022-05-26 15:01:26 +0800

    修改 reggie_project 添加Spring Cache缓存时间设置

--- src/main/java/com/itheima/reggie/ReggieEnterApplication.java
+++ src/main/java/com/itheima/reggie/ReggieEnterApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @Slf4j
 @EnableTransactionManagement
+@EnableCaching
 public class ReggieEnterApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieEnterApplication.class, args);
